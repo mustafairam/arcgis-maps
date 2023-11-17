13c505f59a59393d2bdae6ca7a638b092bf986eb
Mustafa Iram <mustafaudoy@gmail.com>
2023-11-18 01:16:02 +0600

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'arcgis-maps',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
